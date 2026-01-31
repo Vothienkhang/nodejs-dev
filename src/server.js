9c014d68c5c49a134222a93753a357bb8d884683
khangvo95 <khang.vo@getoutabox.com>
2026-01-31 19:14:44 +0700

    install CORS

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
+import cors from 'cors';
 import initWebRoutes from './route/web';
 import connectDB from './config/connectDB';
 
 require('dotenv').config();
 
 let app = express();
+app.use(cors({ 
+    origin: true, 
+    credentials: true,
+}));
 
 //config
 app.use(bodyParser.json());
