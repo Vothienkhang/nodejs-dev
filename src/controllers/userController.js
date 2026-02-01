14a993dc850f2474c6a31f9db5791150a80cc333
khangvo95 <khang.vo@getoutabox.com>
2026-02-01 11:08:59 +0700

    API get all user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -29,6 +29,45 @@ let handleLogin = async (req, res) => {
     });
 }
 
+let handleGetAllUsers = async (req, res) => {
+    try {
+        let id = req.query.id; // All, Id
+
+        if (!id) {
+            return res.status(200).json({
+            errCode: 1,
+            message: "Missing required parameter",
+            users: []
+        });
+    }
+
+        let users = await userService.getAllUsers(id);
+        console.log(users);
+
+        // id req ko phải số nguyên dương
+        if (id !== 'ALL' && users === null) {
+            return res.status(400).json({
+                errCode: 2,
+                message: "Invalid user ID",
+                users: []
+            });
+        }
+        return res.status(200).json({
+            errCode: 0,
+            message: "Get users successfully",
+            users: users
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            errCode: -1,
+            message: "Error from server"
+        });
+    }
+}
+
+
 module.exports = {
-    handleLogin: handleLogin
+    handleLogin: handleLogin,
+    handleGetAllUsers: handleGetAllUsers
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -65,7 +65,44 @@ let checkUserEmail = (email) => {
     });
 }
 
+let getAllUsers = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let users = [];
+
+            // 1) Nếu ko truyền userID hoặc userID === 'ALL' => get all user
+            if (!userId || userId === 'ALL') {
+                users = await db.User.findAll({
+                    attributes: {
+                        exclude: ['password']
+                    }
+                });
+            } 
+            
+            // 2) 
+            else {
+                const id = Number(userId);
+                
+                if (!Number.isInteger(id) || id <= 0) {
+                    return resolve(null); // Return null if userId is not a valid integer or is less than or equal to 0
+                }
+
+                users = await db.User.findOne({
+                    where: { id: userId },
+                    attributes: {
+                        exclude: ['password']
+                    }
+                });
+            }
+
+            resolve(users);
+        } catch (e) {
+            reject(e);
+        }
+    });
+}
 
 module.exports = {
     handleUserLogin: handleUserLogin,
+    getAllUsers: getAllUsers
 }
